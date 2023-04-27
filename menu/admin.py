8b92b14fea6ed19aab0d082f853b3df93618be2e
thalestmm <meier.thales@gmail.com>
2023-04-27 20:07:28 -0300

    basic menu models and admin config

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from .models import *
 
+# Custom Admin Panel actions
+@admin.action(description="Make itens visible on the menu")
+def make_visible(modeladmin, request, queryset):
+    queryset.update(visible=True)
+
+@admin.action(description="Make itens invisible on the menu")
+def make_invisible(modeladmin, request, queryset):
+    queryset.update(visible=False)
+
+
+# Model Admins
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "category", "price", "visible", "gluten_free", "lactose_free", "vegan"]
+    ordering = ["-category", "-visible", "name"]
+    actions = [make_visible, make_invisible]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "visible"]
+    ordering = ["name", "visible"]
+    actions = [make_visible, make_invisible]
+
+
 # Register your models here.
-admin.site.register(MenuCategory)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Ingredient)
-admin.site.register(MenuItem)
+admin.site.register(Item, ItemAdmin)
--- menu/models.py
+++ menu/models.py
@@ -1,24 +1,34 @@
 from django.db import models
 
 # Create your models here.
-class MenuCategory(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
     visible = models.BooleanField(default=True)
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
-class MenuItem(models.Model):
+    def __str__(self) -> str:
+        return self.name
+
+class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
     photo = models.ImageField(max_length=100, blank=True, null=True) # TODO: Allow multiple photos
     price = models.DecimalField(max_digits=6, decimal_places=2) # TODO: Implement discounts (happy hour etc.)
     
-    category = models.ForeignKey(MenuCategory, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     visible = models.BooleanField(default=True)
 
-    ingredients = models.ManyToManyField(Ingredient) # TODO: Allow the customer to remove certain ingredients
+    ingredients = models.ManyToManyField(Ingredient, null=True, blank=True, default=None) # TODO: Allow the customer to remove certain ingredients
     # TODO: implement additional items with a specific price tag
 
     # Item information
@@ -27,3 +37,5 @@ class MenuItem(models.Model):
     vegan = models.BooleanField(default=False)
     # TODO: Maybe implement nutritional macros in the future
 
+    def __str__(self) -> str:
+        return self.name
