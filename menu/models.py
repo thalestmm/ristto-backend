264b7963a1fbbcfc3ab5d4d1bbb4188cca81b89e
thalestmm <meier.thales@gmail.com>
2023-04-28 23:04:32 -0300

    media upload handling and requirements

--- menu/models.py
+++ menu/models.py
@@ -18,25 +18,33 @@ class Item(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000, blank=True, null=True)
-    photo = models.ImageField(max_length=100, blank=True, null=True) 
-    # TODO: Implement storing and serving the images
+    photo = models.ImageField(max_length=100, blank=True, null=True, upload_to='menu') 
     price = models.DecimalField(max_digits=6, decimal_places=2) 
     # TODO: Implement discounts (happy hour etc.) -> This could be another category (and be limited to a certain time of day)
     
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     visible = models.BooleanField(default=True)
 
-    # TODO: Allow the customer to remove certain ingredients (add an observations field)
-    # TODO: For plates with meat, allow them to set the desired point
+    # TODO: Allow the customer to remove certain ingredients -> add an observations field
+    # ? For plates with meat, allow them to set the desired point
 
     # Item information
     lactose_free = models.BooleanField(default=False)
     gluten_free = models.BooleanField(default=False)
     vegan = models.BooleanField(default=False)
-    # TODO: Maybe implement nutritional macros in the future
+    # ? Maybe implement nutritional macros in the future
 
     def __str__(self) -> str:
         return str(self.id) + " - " + self.name
+
+    def save(self, *args, **kwargs):
+        # Save the uploaded photo with a standardized filename
+        if self.photo:
+            photo_extension = "." + self.photo.name.split(".")[1]
+            self.photo.name = str(self.id) + photo_extension
+            # FIXME: Delete the unused uploaded photos from the media root when the models is saved
+        super().save(*args, **kwargs)
+
 
     class Meta:
         ordering = ["-category", "name", "visible"]
--- ristto/urls.py
+++ ristto/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -25,3 +28,8 @@ urlpatterns = [
 
     # Local apps
 ]
+
+# Serve media files in development server
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# TODO: setup for production (https://testdriven.io/blog/django-static-files/)
